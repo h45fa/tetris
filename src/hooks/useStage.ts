66bc0d61f8928e6661dfb754229079a3dfd1424c
Crowley <handhfa45@gmail.com>
2023-05-06 21:19:19 +0300

    create custom hooks

---
+++ src/hooks/useStage.ts
@@ -0,0 +1,28 @@
+import React from "react";
+import { createStage } from "../gameHelpers";
+
+import type { PLAYER } from "./usePlayer";
+
+export type STAGECELL = [ string | number, string];
+export type STAGE = STAGECELL[][];
+
+export const useStage = (player: PLAYER, resetPlayer: () => void) => {
+    const [stage, setStage] = React.useState(createStage());
+
+    React.useEffect(() => {
+        if(!player.pos) return;
+
+        const updateStage = (prevStage: STAGE): STAGE => {
+            const newStage = prevStage.map(row => row.map(cell => (cell[1] === "clear" ? [0, 'clear']: cell)) as STAGECELL[]);
+            player.tetromino.forEach((row, y) => {
+                row.forEach((value, x) => {
+                    if (value !== 0) {
+                        newStage[y + player.pos.y][x + player.pos.x] = [value, `${player.collided ? "merged" : "clear"}`];
+                    }
+                });
+            });
+            return newStage;
+        };
+        setStage(prev => updateStage(prev));
+    }, [player.collided, player.pos?.x, player.pos?.y, player.tetromino]);
+};
