ffc9a92af1eb82650039211d14efcbcfd4416081
Crowley <handhfa45@gmail.com>
2023-05-06 22:44:39 +0300

    collision

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStage } from './gameHelpers';
+import { createStage, isColliding } from './gameHelpers';
 
 //hooks
 import { useInterval } from './hooks/useInterval';
@@ -22,7 +22,9 @@ const App: React.FC = () => {
   const {stage, setStage} = useStage(player,resetPlayer);
 
   const movePlayer = (dir:number) => {
-    updatePlayerPos({ x:dir, y:0, collided: false});
+    if(!isColliding(player, stage, {x: dir, y:0})) {
+      updatePlayerPos({ x:dir, y:0, collided: false});
+    }
   };
 
   const keyUp = ({ keyCode }: {keyCode: number}): void => {
@@ -53,7 +55,16 @@ const App: React.FC = () => {
   };
 
   const drop = (): void => {
-    updatePlayerPos({x: 0,y: 1,collided: false });
+    if(!isColliding(player, stage, {x: 0, y:1})) {
+      updatePlayerPos({x: 0,y: 1,collided: false });
+    } else {
+      if(player.pos.y < 1) {
+        console.log("Game Over");
+        setGameOver(true);
+        setDropTime(null);
+      }
+      updatePlayerPos({ x: 0, y: 0, collided:true});
+    }
   };
 
   useInterval(() => {
